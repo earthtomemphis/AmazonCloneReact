42c166ccce12bbd903e5be328ce762b67a20aa75
Memphis DiAngelis <memphisdiangelis@Memphiss-MacBook-Air.local>
2021-03-24 02:02:10 -0600

    added navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxIHCV9N6whi2NasLBpwCHYciAtLcttVo",
+  authDomain: "clone-f7b2f.firebaseapp.com",
+  projectId: "clone-f7b2f",
+  storageBucket: "clone-f7b2f.appspot.com",
+  messagingSenderId: "605961114034",
+  appId: "1:605961114034:web:b0f9d0e7f2bca4de0494ab"
+};
